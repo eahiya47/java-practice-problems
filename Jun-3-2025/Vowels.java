559b858d2bb92e8d8e217e73531985334386d4f2
Eahiya <eahiya007@gmail.com>
2025-06-03 12:40:57 +0530

    Vowels

--- Jun-3-2025/Vowels.java
+++ Jun-3-2025/Vowels.java
@@ -1,5 +1,46 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Vowels {
-    public static void main(String[] args) {
-        System.out.println("demo");
+
+    public static ArrayList<String> print(String str){
+
+        ArrayList<String> ans = new ArrayList<>();
+        String lower = str.toLowerCase();
+        String temp = "";
+        char[] ch = lower.toCharArray();
+
+        for(int i = 0; i < lower.length(); i++){
+            if(ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'u' || ch[i] == 'o' || ch[i] == 'i'){
+                if(temp != ""){
+                    ans.add(temp);
+                }
+                temp = "";
+                continue;
+            }else{
+                temp = temp + ch[i];
+            }
+        }
+
+        if(temp != ""){
+            ans.add(temp);
+        }
+
+        return ans;
+
+    }
+    
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        ArrayList<String> list = print(str);
+        System.out.println(list);
+        
+        sc.close();
+
     }
 }
