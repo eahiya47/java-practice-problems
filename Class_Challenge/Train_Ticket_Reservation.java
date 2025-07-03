f9d504ec958d9a950fe6a4295b2b6e90c56628f2
Eahiya <eahiya007@gmail.com>
2025-07-03 16:14:10 +0530

    updated

--- Class_Challenge/Train_Ticket_Reservation.java
+++ Class_Challenge/Train_Ticket_Reservation.java
@@ -3,13 +3,13 @@ package Class_Challenge;
 import java.util.*;
 class Node{
     String passengerName;
-    int passengerId;
-    String seatNo;
+    int trainId;
+    String pnr;
     Node next;
     
-    Node(String passengerName, int passengerId){
+    Node(String passengerName, int trainId){
         this.passengerName = passengerName;
-        this.passengerId = passengerId;
+        this.trainId = trainId;
         next = null;
     }
 }
@@ -18,13 +18,16 @@ class LinkedList{
     static Scanner sc = new Scanner(System.in);
     static Node head = null;
     
-    public static void bookTicket(String passengerName, int passengerId){
-        System.out.println("Ticket booked successfully for " + passengerName + "\nClick ENTER to continue.");
-        sc.nextLine();
-        Node node = new Node(passengerName, passengerId);
+    public static void bookTicket(String passengerName, int trainId){
+        Node node = new Node(passengerName, trainId);
+        long num = 1000000000L + (long)(Math.random() * 9000000000L);
+        node.pnr = String.valueOf(num);
         
         if(head == null){
             head = node;
+            System.out.println("Ticket booked successfully!");
+            System.out.println("\nName : " + passengerName + "\nPNR : " + node.pnr + "\nClick ENTER to continue.");
+            sc.nextLine();
             return;
         }
         
@@ -34,12 +37,13 @@ class LinkedList{
             current = current.next;
         }
         current.next = node;
-        System.out.println("Ticket booked successfully for " + passengerName + "\nClick ENTER to continue.");
+        System.out.println("Ticket booked successfully!");
+        System.out.println("\nName : " + passengerName + "\nPNR : " + node.pnr + "\nClick ENTER to continue.");
         sc.nextLine();
         return;
     }
     
-    public static void cancelTicket_by_ID(int passengerId){
+    public static void cancelTicket_by_PNR(String pnr){
         
         if(head == null){
             System.out.println("No reservations done!\nClick ENTER to continue.");
@@ -47,7 +51,7 @@ class LinkedList{
             return;
         }
         
-        if(head.passengerId == passengerId){
+        if(head.pnr.equals(pnr)){
             System.out.println("Ticket cancelled successfully!\nClick ENTER to continue.");
             sc.nextLine();
             return;
@@ -55,12 +59,12 @@ class LinkedList{
 
         Node current = head;
 
-        while(current.next != null && current.next.passengerId != passengerId){
+        while(current.next != null && current.next.pnr.equals(pnr)){
             current = current.next;
         }
 
         if(current.next == null){
-            System.out.println("Passenger ID not found!\nClick ENTER to continue.");
+            System.out.println("PNR not found!\nClick ENTER to continue.");
             sc.nextLine();
             return;
         }
@@ -69,6 +73,57 @@ class LinkedList{
         System.out.println("Ticket cancelled successfully!\nClick ENTER to continue.");
         sc.nextLine();
         return;
+    }
+
+    public static void cancelTicket_by_Name(String name){
+
+        int count  = 0;
+
+        Node temp = head;
+        while (temp != null) {
+            if (temp.passengerName.equals(name)) {
+                count++;
+            }
+            temp = temp.next;
+        }
+
+        if(count == 0){
+            System.out.println("No reservations done!\nClick ENTER to continue.");
+            sc.nextLine();
+            return;
+        }
+
+        else if(count == 1)
+        {
+            if(head.passengerName.equals(name)){
+                head = head.next;
+                System.out.println("Ticket cancelled successfully!\nClick ENTER to continue.");
+                sc.nextLine();
+                return;
+            }
+
+            Node current = head;
+
+            while(current.next != null && current.next.passengerName.equals(name)){
+                current = current.next;
+            }
+
+            if(current.next == null){
+                System.out.println("No registration in this name!\nClick ENTER to continue.");
+                sc.nextLine();
+                return;
+            }
+
+            current.next = current.next.next;
+            System.out.println("Ticket cancelled successfully!\nClick ENTER to continue.");
+            sc.nextLine();
+            return;
+        }else if(count > 1){
+            System.out.print("Multiple passengers are found in this name, Please enter PNR to reduce the inconvenience! ");
+            String pnr = sc.next();
+            cancelTicket_by_PNR(pnr);
+        }
+
     }
 
     public static void displayTickets(){
@@ -83,7 +138,8 @@ class LinkedList{
 
         while(current != null){
             System.out.println("Passenger Name : " + current.passengerName);
-            System.out.println("Passenger ID : " + current.passengerId);
+            System.out.println("Train ID : " + current.trainId);
+            System.out.println("PNR : " + current.pnr);
             System.out.println();
             current = current.next;
             System.out.println("Click ENTER to continue.");
@@ -99,7 +155,7 @@ class Main{
         System.out.println("\nTrain Ticket Reserving Platform");
         
         while(true){
-            System.out.println("\n1.Book Ticket \n2.Cancel Ticket \n3.View Tickets \n4.Exit menu\n");
+            System.out.println("\n1.Book Ticket \n2.Cancel Ticket by Name\n3.Cancel Ticket by PNR \n4.View Tickets \n5.Exit menu\n");
             System.out.print("Enter your choice of service : ");
             int choice = sc.nextInt();
             sc.nextLine();
@@ -114,17 +170,24 @@ class Main{
                     break;
                 
                 case 2:
-                    System.out.print("Enter the id to cancel ticket : ");
-                    id = sc.nextInt();
+                    System.out.print("Enter the Passenger Name to cancel ticket : ");
+                    String Pname = sc.next();
 
-                    LinkedList.cancelTicket_by_ID(id);
+                    LinkedList.cancelTicket_by_Name(Pname);
                     break;
 
                 case 3:
-                    LinkedList.displayTickets();
+                    System.out.print("Enter the Passenger PNR to cancel ticket : ");
+                    String pnr = sc.next();
+
+                    LinkedList.cancelTicket_by_PNR(pnr);
                     break;
 
                 case 4:
+                    LinkedList.displayTickets();
+                    break;
+
+                case 5:
                     System.out.println("Thank You!");
                     return;
                 
